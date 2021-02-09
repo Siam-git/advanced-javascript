494755f371feba87929673c27739a670f443ae5c
Siam-git <siam9850@gmail.com>
2021-02-09 10:31:19 +0600

    javascript array map

---
+++ map-filter.js
@@ -0,0 +1,21 @@
+const numbers = [3, 4, 5, 6, 7, 8];
+const output = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+
+// }
+// // console.log(output);
+
+// function square(element) {
+//     return element * element;
+// }
+
+// const result = numbers.map(function(element) { //element, index, array
+//     return element * element;
+// })
+const result = numbers.map(x => x * x);
+console.log(result);
