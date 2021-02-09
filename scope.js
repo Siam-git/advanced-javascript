766a1e78639db129e95b9243e9a5f007366b10fc
Siam-git <siam9850@gmail.com>
2021-02-09 12:16:33 +0600

    closure

--- scope.js
+++ scope.js
@@ -1,7 +1,20 @@
 function sum(first, second) {
-    let result = first + second;
+    let result = first + second + bonus;
+    return result;
+
+    if (result > 9) {
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        modd = "cranky";
+
+    }
+
+    console.log(day);
+    let day = "friday";
+
     return result;
 }
 
 const output = sum(3, 7);
-console.log(output);
+//console.log(output);
