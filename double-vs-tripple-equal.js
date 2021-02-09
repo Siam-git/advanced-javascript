df081a1ff1b455847fa2144fcf08bb7bcc638114
Siam-git <siam9850@gmail.com>
2021-02-09 10:12:34 +0600

    double vs tripple equal problems

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,9 @@
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+// == stands for comparing common elements, === is strict comparing every elements. === is more accurate
