e5572b6dc180fe0bcbd95018d2630beb5b544a14
Siam-git <siam9850@gmail.com>
2021-02-09 10:02:46 +0600

    null vs undefined practice

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+//undefined print practice
+let pakhi;
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const result = add(13, 82);
+console.log(result);
+
+const him = { name: "Shahrukh Khan", phone: 252565658 };
+console.log(him.address); //it'll show undefined
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 3, 5];
+console.log(ages[12]);
