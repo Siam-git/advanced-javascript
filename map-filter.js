949ec905c3a0f2492207702cb3a6111e1c583e7b
Siam-git <siam9850@gmail.com>
2021-02-09 10:36:14 +0600

    map filter find

--- map-filter.js
+++ map-filter.js
@@ -17,5 +17,13 @@ const output = [];
 // const result = numbers.map(function(element) { //element, index, array
 //     return element * element;
 // })
-const result = numbers.map(x => x * x);
-console.log(result);
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+//filter
+const bigger = numbers.filter(x => x < 5);
+
+const isThere = numbers.find(x => x > 5);
+
+console.log(isThere);
+//map gives array, filter gives array, find gives element
